a94cfe613fdf218a6aebdc06c3a305fb3084e690
soumanpaul <souman1729@gmail.com>
2025-02-28 23:49:38 +0530

    added card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 console.log("im Server component");
 
 export default async function Home({ searchParams} : { 
   searchParams: Promise<{ query?: string }> }) {
     const query = (await searchParams).query;
+
+    const posts = [{
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description",
+      image: 'https://t4.ftcdn.net/jpg/02/10/96/95/360_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg',
+      category: "Robots",
+      title: "We Robots"
+    }];
+
   return (
     <>
       <section className="pink_container">
@@ -12,6 +25,17 @@ export default async function Home({ searchParams} : {
         <p className="sub-heading !max-w-3xl"> 
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
         <SearchForm query={query}  />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest Pitches"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length > 0 ? posts.map((post, index) => (
+              <StartupCard key={post?._id} post={post} />
+          )) : <p className="no-results">No posts found</p>}
+        </ul>
       </section>
     </>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formateDate(date: string) {
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString('en-us', options);
+}
